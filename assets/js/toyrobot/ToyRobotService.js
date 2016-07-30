f3b94846e6182bb9519bf97cd1e292ef23bd9356
jeffyyyy <zy53work@gmail.com>
2016-07-31 11:59:31 +1200

    add patch commands mode, which put a list of commands together and calculate a final output

--- assets/js/toyrobot/ToyRobotService.js
+++ assets/js/toyrobot/ToyRobotService.js
@@ -25,6 +25,16 @@ ToyRobot.service('ToyRobotService', ['$http', '$q', function($http, $q) {
     return defer.promise;
   };
 
+  toyrobotService.executePatchCommands = function(data) {
+    var defer = $q.defer();
+    $http.post('/api/handlePatchCommands', data).success(function(response) {
+      defer.resolve(response);
+    }).error(function(err) {
+      defer.reject(err);
+    });
+    return defer.promise;
+  };
+
   return toyrobotService;
 
 }]);
