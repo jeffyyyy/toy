40dbbc70a229ac8d54bc632ca8ae3295a39a57c4
Jeff Yu <jeff.yu@sparkventures.co.nz>
2016-08-02 10:49:02 +1200

    Clean up install guide, use makefile and grunt to install all global packages

---
+++ tasks/config/make.js
@@ -0,0 +1,14 @@
+/**
+ * `make`
+ *
+ * ---------------------------------------------------------------
+ *
+ * Include grunt-make module
+ *
+ * For usage docs see:
+ *   https://github.com/gruntjs/grunt-make
+ *
+ */
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-make');
+};
